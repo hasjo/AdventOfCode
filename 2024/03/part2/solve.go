ffaefcb6f68b108eaf38621e0dc4de3d721f479a
Jordan Haskovec <jorhaskovec@protonmail.com>
2024-12-03 11:20:51 -0600

    03-1 complete partial 03-2

---
+++ 2024/03/part2/solve.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+        if err != nil {
+		log.Fatal("FAILURE TO OPEN INPUT")
+	}
+        mulMatch := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
+        foundSlice := mulMatch.FindAll(data, -1)
+        // indexSlice := mulMatch.FindAllIndex(data,-1)
+        doMatch := regexp.MustCompile(`do\(\)`)
+        doSlice := doMatch.FindAllIndex(data, -1)
+        fmt.Println(doSlice)
+        dontMatch := regexp.MustCompile(`don\'t\(\)`)
+        dontSlice := dontMatch.FindAllIndex(data, -1)
+        fmt.Println(dontSlice)
+        totalMul := 0
+        for _, match := range(foundSlice){
+            matchStr := string(match)
+            workStr := strings.Replace(matchStr, "mul(", "", 1)
+            workStr = strings.Replace(workStr, ")", "", 1)
+            values := strings.SplitN(workStr, ",", 2)
+            value1, err := strconv.Atoi(values[0])
+            if err != nil {
+                log.Fatal("HEY CANT CONVERT:", values[0])
+            }
+            value2, err := strconv.Atoi(values[1])
+            if err != nil {
+                log.Fatal("HEY CANT CONVERT:", values[1])
+            }
+            newVal := value1 * value2
+            totalMul += newVal
+        }
+        fmt.Println(totalMul)
+}
